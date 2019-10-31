34c94e30537584362a029caef7b2be999536012e
hotatekaoru <kaorupochi@gmail.com>
2019-11-01 02:15:42 +0900

    :+1: メイン画面のActivityを用意し、AppBarを配置

---
+++ app/src/main/java/com/hotatekaoru/kotlincalculator/view/CalculatorActivity.kt
@@ -0,0 +1,14 @@
+package com.hotatekaoru.kotlincalculator.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hotatekaoru.kotlincalculator.R
+
+class CalculatorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculator)
+
+    }
+}
