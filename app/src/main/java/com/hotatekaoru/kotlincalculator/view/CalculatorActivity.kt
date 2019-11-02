45f1e026d4490265db629b9dc5cccf856aceb17c
hotatekaoru <kaorupochi@gmail.com>
2019-11-03 03:34:35 +0900

    :hammer: mainValueTextをEditTextからTextViewに変更

--- app/src/main/java/com/hotatekaoru/kotlincalculator/view/CalculatorActivity.kt
+++ app/src/main/java/com/hotatekaoru/kotlincalculator/view/CalculatorActivity.kt
@@ -7,7 +7,6 @@ import androidx.lifecycle.ViewModelProviders
 import com.hotatekaoru.kotlincalculator.R
 import com.hotatekaoru.kotlincalculator.databinding.ActivityCalculatorBinding
 import com.hotatekaoru.kotlincalculator.viewmodel.CalculatorViewModel
-import kotlinx.android.synthetic.main.activity_calculator.*
 
 class CalculatorActivity : AppCompatActivity() {
 
@@ -19,8 +18,5 @@ class CalculatorActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_calculator)
         viewModel = ViewModelProviders.of(this).get(CalculatorViewModel::class.java)
         binding.viewmodel = viewModel
-
-        // editTextタップ時にキーボードを非表示にする
-        mainValueEditText.showSoftInputOnFocus = false
     }
 }
