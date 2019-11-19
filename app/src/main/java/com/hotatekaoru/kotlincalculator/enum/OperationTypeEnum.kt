5c19d68ccff8025b6b803beb1a517b363d995725
hotatekaoru <kaorupochi@gmail.com>
2019-11-20 01:20:37 +0900

    :+1: 計算ロジックを実装

--- app/src/main/java/com/hotatekaoru/kotlincalculator/enum/OperationTypeEnum.kt
+++ app/src/main/java/com/hotatekaoru/kotlincalculator/enum/OperationTypeEnum.kt
@@ -1,8 +1,16 @@
 package com.hotatekaoru.kotlincalculator.enum
 
 enum class OperationTypeEnum(val label: String) {
-    PLUS("+"),
-    MINUS("-"),
-    MULTIPLE("×"),
-    DIVIDE("÷")
+    PLUS("+") { override fun calc(x: Double, y: Double): Double = x + y },
+    MINUS("-") { override fun calc(x: Double, y: Double): Double = x - y },
+    MULTIPLE("×") { override fun calc(x: Double, y: Double): Double = x * y },
+    DIVIDE("÷") { override fun calc(x: Double, y: Double): Double = x / y };
+
+    companion object {
+        fun isPlusOrMinus(enum: OperationTypeEnum): Boolean {
+            return PLUS == enum || MINUS == enum
+        }
+    }
+
+    abstract fun calc(x: Double, y: Double): Double
 }
--- app/src/main/java/com/hotatekaoru/kotlincalculator/model/Calculator.kt
+++ app/src/main/java/com/hotatekaoru/kotlincalculator/model/Calculator.kt
@@ -3,18 +3,23 @@ package com.hotatekaoru.kotlincalculator.model
 import com.hotatekaoru.kotlincalculator.enum.OperationTypeEnum
 
 class Calculator(private val formula: String) {
-    private val numberList = ArrayList<String>()
+    private val numberStringList = ArrayList<String>()
+    private val numberList = ArrayList<Double>()
     private val operationList = ArrayList<OperationTypeEnum>()
     private var isLastCharOperation = true
 
     fun call(): Double {
-        // listに格納
+        // formulaを、numberStringListとoperationListに格納
         setFormulaToLists()
+
+        // numberStringListをDouble型に変換してnumberListに格納。その際、引き算がある場合はその後の数値を-1倍する
+        convertToNumberList()
 
         // 掛け算・割り算を実施
         calcMultiplicationAndDivision()
+
         // 足し算・引き算を実施
-        return 1.23
+        return calcAddAndMinus()
     }
 
     /**
@@ -26,27 +31,27 @@ class Calculator(private val formula: String) {
             // TODO: ここはもっといい書き方があるのかもしれない
             if (char in '0'..'9') {
                 if (isLastCharOperation) {
-                    numberList.add(char.toString())
+                    numberStringList.add(char.toString())
                 } else {
                     val newNumber = "${numberList.last()}$char"
-                    numberList.removeAt(numberList.lastIndex)
-                    numberList.add(newNumber)
+                    numberStringList.removeAt(numberList.lastIndex)
+                    numberStringList.add(newNumber)
                 }
                 isLastCharOperation = false
             } else if (char.toString() == ".") {
                 if (isLastCharOperation) {
-                    numberList.add("0.")
+                    numberStringList.add("0.")
                 } else {
-                    val newNumber = "${numberList.last()}$char"
-                    numberList.removeAt(numberList.lastIndex)
-                    numberList.add(newNumber)
+                    val newNumber = "${numberStringList.last()}$char"
+                    numberStringList.removeAt(numberStringList.lastIndex)
+                    numberStringList.add(newNumber)
                 }
                 isLastCharOperation = false
             } else {
                 // 最初にマイナスから始まる場合は、最初に0があたかもあるかのようにする。
                 // これにより、numberList[0]のあとに、operationList[0]がある状態を作る
-                if (char.equals(OperationTypeEnum.MINUS.label) && numberList.isEmpty()) {
-                    numberList.add("0")
+                if (OperationTypeEnum.MINUS.label == char.toString() && numberStringList.isEmpty()) {
+                    numberStringList.add("0")
                 }
                 OperationTypeEnum.values().firstOrNull { it.label == char.toString() }?.let {
                     operationList.add(it)
@@ -56,6 +61,27 @@ class Calculator(private val formula: String) {
         }
     }
 
+    private fun convertToNumberList() {
+        for ((index, string) in numberStringList.withIndex()) {
+            if (index == 0 || OperationTypeEnum.MINUS != operationList[index - 1]) {
+                numberList.add(string.toDouble())
+            } else {
+                numberList.add(string.toDouble() * -1)
+            }
+        }
+    }
+
     private fun calcMultiplicationAndDivision() {
+        for ((index, operation) in operationList.withIndex()) {
+            if (OperationTypeEnum.isPlusOrMinus(operation)) { continue }
+
+            val value = operation.calc(numberList[index], numberList[index + 1])
+            numberList[index] = 0.0
+            numberList[index + 1] = value
+        }
+    }
+
+    private fun calcAddAndMinus(): Double {
+        return numberList.sum()
     }
 }
